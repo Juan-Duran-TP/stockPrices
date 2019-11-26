607b91aefc0e75e05a97db1cbedfc0743a0bb723
foodfighterdevs <57101058+foodfighterdevs@users.noreply.github.com>
2019-11-26 15:55:31 -0600

    removed unnecessary files

--- stockCalcDriver.js
+++ stockCalcDriver.js
@@ -3,11 +3,19 @@ const getPrices = require('./getPrices.js')
 let min = "1min";
 
 let priceArray = [];
-getPrices().then(data => {
-    let item;
-    for (const timestamp in data[`Time Series (${min})`]) {
-      item = data[`Time Series (${min})`][timestamp]["4. close"];
-      priceArray.push(parseFloat(item));
-    }
-    console.log(getMaxProfit(priceArray));
-});
+// getPrices().then(data => {
+//     let item;
+//     for (const timestamp in data[`Time Series (${min})`]) {
+//       item = data[`Time Series (${min})`][timestamp]["4. close"];
+//       priceArray.push(parseFloat(item));
+//     }
+//     console.log(getMaxProfit(priceArray));
+// });
+
+const prices = await getPrices();
+let item;
+for (const timestamp in prices[`Time Series (${min})`]) {
+  item = prices[`Time Series (${min})`][timestamp]["4. close"];
+  priceArray.push(parseFloat(item));
+}
+console.log(getMaxProfit(priceArray));
